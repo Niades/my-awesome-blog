cafaa54f0dfbe081baf61b65957776e59bd48c0f
Daniil Polovinkin <dolovinkin@gmail.com>
2016-11-18 12:38:56 +0300

    Feature: Список записей на главной

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -1,1 +1,9 @@
-export default () => {};
+const MainController = ($scope, Restangular) => {
+    Restangular
+        .all('blog-entry')
+        .getList()
+        .then((entries) => $scope.entries = entries);
+};
+
+
+export default MainController;
